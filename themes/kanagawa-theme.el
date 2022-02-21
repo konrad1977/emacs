cf0f2460a898d1c62a2eb091d95f521375a63402
Mikael Konradsson <mikael.konradsson@bontouch.com>
2022-02-21 14:26:34 +0100

    Better flycheck style

--- themes/kanagawa-theme.el
+++ themes/kanagawa-theme.el
@@ -273,9 +273,9 @@
   (flycheck-error-list-warning                   (:foreground roninYellow :bold t))
   (flycheck-error-list-error                     (:foreground samuraiRed :bold t))
   (flycheck-error-list-info                      (:foreground waveAqua1 :bold t))
-  (flycheck-inline-error                         (:foreground samuraiRed :background winterRed :italic t))
-  (flycheck-inline-info                          (:foreground lightBlue :background winterBlue :italic t))
-  (flycheck-inline-warning                       (:foreground roninYellow :background winterYellow :italic t))
+  (flycheck-inline-error                         (:foreground samuraiRed :background winterRed :italic t :height 128))
+  (flycheck-inline-info                          (:foreground lightBlue :background winterBlue :italic t :height 128))
+  (flycheck-inline-warning                       (:foreground roninYellow :background winterYellow :italic t :height 128))
 
   ;; indent dots
   (highlight-indent-guides-character-face        (:foreground sumiInk-3))
