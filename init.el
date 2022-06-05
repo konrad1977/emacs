2312613682320c75f3feb882e3deaf49e69f947e
Mikael Konradsson <mikael.konradsson@bontouch.com>
2022-06-05 09:52:10 +0200

    Alot of small updates to the init file.

--- init.el
+++ init.el
@@ -761,13 +761,13 @@
 (use-package git-gutter
   :hook (prog-mode . git-gutter-mode)
   :config
-  (setq git-gutter:update-interval 0.2))
+  (setq git-gutter:update-interval 0.1))
 
 (use-package git-gutter-fringe
   :config
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [224] nil nil '(center repeated)))
 
 (use-package forge
   :commands forge-pull
