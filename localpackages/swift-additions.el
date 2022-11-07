30e8a193f2c1ddc37daad87943b823043ddc956e
Mikael Konradsson <mikael.konradsson@bontouch.com>
2022-11-07 18:46:14 +0100

    Changed to using completing-read for selecting build targets and     schemes. Fixed inhihit-messages för async/sync processes.

--- localpackages/swift-additions.el
+++ localpackages/swift-additions.el
@@ -41,16 +41,6 @@
 (defvar local-device-id nil)
 (defvar DEBUG nil)
 
-(defun get-booted-simulator ()
-  "Get booted simulator if any."
-  (let ((device-id (shell-command-to-string get-booted-simulator-command)))
-    (if (not (string= "" device-id))
-        (clean-up-newlines device-id)
-      nil)))
-
-(defun command-string-to-list (cmd)
-  "Split the CMD unless it is a list.  This function respects quotes."
-  (if (listp cmd) cmd (split-string-and-unquote cmd)))
 
 (cl-defun async-shell-command-to-string (&key process-name &key command &key callback)
   "Execute shell command COMMAND asynchronously in the background.
@@ -96,23 +86,36 @@ ARGS are rest arguments, appended to the argument list."
   (with-temp-buffer
     (unless (zerop
              (do-call-process executable
-                                         nil
-                                         ;; Disregard stderr output, as it
-                                         ;; corrupts JSON.
-                                         (list t nil)
-                                         nil
-                                         args))
+                              nil
+                              ;; Disregard stderr output, as it
+                              ;; corrupts JSON.
+                              (list t nil)
+                              nil
+                              args))
       (error "%s: %s %s" "Cannot invoke executable" executable (buffer-string) default-directory))
     (goto-char (point-min))
     (json-read)))
 
+(defun get-booted-simulator ()
+  "Get booted simulator if any."
+  (let ((device-id (shell-command-to-string get-booted-simulator-command)))
+    (if (not (string= "" device-id))
+        (clean-up-newlines device-id)
+      nil)))
+
+(defun command-string-to-list (cmd)
+  "Split the CMD unless it is a list.  This function respects quotes."
+  (if (listp cmd) cmd (split-string-and-unquote cmd)))
+
 (defun start-simulator-with-id (id)
   "Launch a specific simulator with (as ID)."
-  (call-process-shell-command (format "open --background -a simulator --args -CurrentDeviceUDID %s" id)))
+  (inhibit-sentinel-messages
+   #'call-process-shell-command (format "open --background -a simulator --args -CurrentDeviceUDID %s" id)))
 
 (defun boot-simuator-with-id (id)
   "Simulator app is running.  Boot simulator (as ID)."
-  (call-process-shell-command (format "xcrun simctl boot %s" id)))
+  (inhibit-sentinel-messages
+   #'call-process-shell-command (format "xcrun simctl boot %s" id)))
 
 (defun is-simulator-app-running ()
   "Check if simulator is running."
@@ -137,7 +140,7 @@ ARGS are rest arguments, appended to the argument list."
       (if simulator-name
           (setq current-simulator-name (format "%s(simulator)" simulator-name))
         (setq current-simulator-name "Simulator (unknown)"))))
-    current-simulator-name)
+  current-simulator-name)
 
 (defun fetch-targets ()
   "Select the target."
@@ -176,7 +179,7 @@ ARGS are rest arguments, appended to the argument list."
       (let ((device-id
              (or (get-booted-simulator)
                  (build-simulator-menu :title "Choose a simulator:" :list (swift-additions:list-available-simulators)))))
-                 ;; (widget-choose "Choose a simulator:" (swift-additions:list-available-simulators)))))
+        ;; (widget-choose "Choose a simulator:" (swift-additions:list-available-simulators)))))
         (progn
           (setup-simulator-dwim current-simulator-id)
           (setq current-simulator-id device-id)))))
@@ -188,14 +191,14 @@ ARGS are rest arguments, appended to the argument list."
     (setq current-project-root project))
   (if (not
        (string= current-project-root project))
-       (progn
-         (swift-additions:reset-settings)
-         (setq current-project-root project))))
+      (progn
+        (swift-additions:reset-settings)
+        (setq current-project-root project))))
 
 (defun xcodebuild-command ()
   "Use x86 environement."
   (if current-environment-x86
-   "env /usr/bin/arch -x86_64 xcrun xcodebuild build \\"
+      "env /usr/bin/arch -x86_64 xcrun xcodebuild build \\"
     "xcrun xcodebuild build\\"))
 
 (defun get-build-folder ()
@@ -231,10 +234,11 @@ ARGS are rest arguments, appended to the argument list."
    (if simulatorId
        (format "-destination 'generic/platform=iOS Simulator,id=%s' \\" simulatorId)
      (format "-destination 'generic/platform=iOS' \\" ))
-   "-skipUnavailableActions \\"
    "-destination-timeout 1 \\"
+   "-skipPackageUpdates \\"
    "-scmProvider system \\"
    "-parallelizeTargets \\"
+   "-hideShellScriptEnvironment \\"
    "-packageCachePath ~/Library/Cache/com.apple.swiftpm \\"
    "-quiet \\"
    "-derivedDataPath build"))
@@ -247,9 +251,9 @@ ARGS are rest arguments, appended to the argument list."
 (defun get-index-store-path ()
   "Get the index store path."
   (let ((index-store-path (concat current-project-root "build/Index/DataStore")))
-        (if (file-directory-p index-store-path)
-            index-store-path
-          nil)))
+    (if (file-directory-p index-store-path)
+        index-store-path
+      nil)))
 
 (cl-defun parse-errors-from (&key text)
   "Parse errors from TEXT."
@@ -273,15 +277,14 @@ ARGS are rest arguments, appended to the argument list."
      (format "xcrun simctl install %s %s%s.app\n" simulatorID folder (get-app-name-from :folder folder)))))
 
 (defun swift-additions:terminate-all-running-apps ()
-    "Terminate runnings apps."
-    (interactive)
-    (terminate-app-in-simulator :simulatorID current-simulator-id)
-    (terminate-app-in-simulator :simulatorID secondary-simulator-id))
+  "Terminate runnings apps."
+  (interactive)
+  (terminate-app-in-simulator :simulatorID current-simulator-id)
+  (terminate-app-in-simulator :simulatorID secondary-simulator-id))
 
 (cl-defun terminate-app-in-simulator (&key simulatorID)
   "Terminate app that is running in simulator with SIMULATORID."
-  (message-with-color :tag "[Terminating]" :text current-xcode-scheme :attributes 'warning)
-  (call-process-shell-command
+  (inhibit-sentinel-messages #'call-process-shell-command
    (concat
     (if simulatorID
         (format "xcrun simctl terminate %s %s" simulatorID (fetch-or-load-app-identifier))
@@ -323,7 +326,7 @@ ARGS are rest arguments, appended to the argument list."
          old-set-process-sentinel
          process
          `(lambda (&rest args)
-            (let ((inhibit-message t))
+            (cl-letf (((symbol-function 'message) #'ignore))
               (apply (quote ,sentinel) args)))))))
         (apply fun args)))
 
@@ -364,13 +367,13 @@ ARGS are rest arguments, appended to the argument list."
 (cl-defun find-project-root-folder-with (&key extension)
   "Find project folder where it has its project files EXTENSION."
   (if-let* ((project-root (expand-file-name (projectile-project-root)))
-         (root (directory-files project-root nil (format "\\%s$" extension)))
-         (subroot (get-files-from :directory project-root :extension extension :exclude "build"))
-         (workroot (or root subroot))
-         (path (file-name-directory (car-safe workroot))))
-    (if (and path (string-match-p (regexp-quote ".xcodeproj") path))
-        (file-name-directory (directory-file-name path))
-      path)))
+            (root (directory-files project-root nil (format "\\%s$" extension)))
+            (subroot (get-files-from :directory project-root :extension extension :exclude "build"))
+            (workroot (or root subroot))
+            (path (file-name-directory (car-safe workroot))))
+      (if (and path (string-match-p (regexp-quote ".xcodeproj") path))
+          (file-name-directory (directory-file-name path))
+        path)))
 
 (defun get-ios-project-root ()
   "Get the current root of the project."
@@ -395,7 +398,7 @@ ARGS are rest arguments, appended to the argument list."
   "Return the current SDK."
   (if local-device-id
       "iphoneos"
-      "iphonesimulator"))
+    "iphonesimulator"))
 
 (defun swift-additions:install-app-on-device ()
   "Install an app on device."
@@ -412,27 +415,31 @@ ARGS are rest arguments, appended to the argument list."
   (let* ((default-directory current-project-root)
          (simulator-id (fetch-or-load-simulator-id)))
 
-    (swift-additions:terminate-all-running-apps)
     (setq applicationName (get-app-name-from :folder (get-build-folder)))
     (setq simulatorName  (fetch-simulator-name))
-
+    
     (message-with-color
      :tag "[Installing]"
      :text (format "%s onto %s. Will launch app when done." applicationName simulatorName)
      :attributes '(:inherit success))
 
-    (call-process-shell-command (install-app-in-simulator-command :simulatorID simulator-id))
+    (swift-additions:terminate-all-running-apps)
+
+    (inhibit-sentinel-messages #'
+     call-process-shell-command (install-app-in-simulator-command :simulatorID simulator-id))
 
     (when-let ((secondary-id secondary-simulator-id))
       (setup-simulator-dwim secondary-simulator-id)
       (call-process-shell-command (install-app-in-simulator-command :simulatorID secondary-id)))
 
-    (run-async-command-in-buffer
-     :command (launch-app-in-simulator
-               :appIdentifier current-app-identifier
-               :applicationName applicationName
-               :simulatorName simulatorName
-               :simulatorID simulator-id))))
+    (inhibit-sentinel-messages #'async-shell-command
+                               (launch-app-in-simulator
+                                :appIdentifier current-app-identifier
+                                :applicationName applicationName
+                                :simulatorName simulatorName
+                                :simulatorID simulator-id)
+                               xcodebuild-buffer)
+    ))
 
 (defun build-using-compilation-mode ()
   "Build using builtin compile and 'compilation-mode'."
@@ -457,12 +464,11 @@ ARGS are rest arguments, appended to the argument list."
   (message-with-color :tag "[Resetting]" :text "Build configiration" :attributes 'warning))
 
 (defun swift-additions:run-without-compiling ()
-    "Run app in simulator/device without compiling."
-    (interactive)
-    (setq-local inhibit-message t)
-    (periphery-kill-buffer)
-    (swift-additions:kill-xcode-buffer)
-    (swift-additions:run-app))
+  "Run app in simulator/device without compiling."
+  (interactive)
+  (periphery-kill-buffer)
+  (swift-additions:kill-xcode-buffer)
+  (swift-additions:run-app))
 
 (defun swift-additions:compile-and-run-silent ()
   "Build project using xcodebuild."
@@ -470,7 +476,6 @@ ARGS are rest arguments, appended to the argument list."
   (save-some-buffers t)
   (periphery-kill-buffer)
   (swift-additions:kill-xcode-buffer)
-  (setq-local inhibit-message t)
   (setq device-or-simulator "[Building device target]")
   (setq local-device-id (get-connected-device-id))
   (when (not local-device-id)
@@ -486,13 +491,13 @@ ARGS are rest arguments, appended to the argument list."
            :command (build-app-command :simulatorId: current-simulator-id :deviceId local-device-id)
            :callback #'swift-additions:check-for-errors))
         (message-with-color :tag device-or-simulator :text (format "%s. Please wait. Patience is a virtue!" current-xcode-scheme) :attributes 'warning))
-        (if (is-a-swift-package-base-project)
-            (swift-additions:build-swift-package)
-          (messsage "Not xcodeproject nor swift package")
-        )))
+    (if (is-a-swift-package-base-project)
+        (swift-additions:build-swift-package)
+      (messsage "Not xcodeproject nor swift package")
+      )))
 
 (defun swift-additions:test-module-silent ()
-    "Test module."
+  "Test module."
   (interactive)
   (save-some-buffers t)
   (periphery-kill-buffer)
@@ -514,8 +519,8 @@ ARGS are rest arguments, appended to the argument list."
         (progn
           (message-with-color :tag "[Removing]" :text (format "Folder for %s" default-directory) :attributes '(:inherit warning))
           (delete-directory default-directory t nil))
-          (message-with-color :tag "[Failed]" :text (format "Build folder %s doesn't exist" default-directory) :attributes '(:inherit error))))
-    (message-with-color :tag "[Done]" :text "Ready to rumble." :attributes '(:inherit success)))
+      (message-with-color :tag "[Failed]" :text (format "Build folder %s doesn't exist" default-directory) :attributes '(:inherit error))))
+  (message-with-color :tag "[Done]" :text "Ready to rumble." :attributes '(:inherit success)))
 
 (defun swift-additions:buffer-contains-substring (string)
   "Check if buffer contain (as STRING)."
@@ -526,7 +531,7 @@ ARGS are rest arguments, appended to the argument list."
 
 (defun swift-additions:functions-and-pragmas ()
   "Show swift file compressed functions and pragmas."
-   (interactive)
+  (interactive)
   (let ((list-matching-lines-face nil))
     (occur "\\(func\\)\\|\\(#pragma mark\\)\\|\\(MARK:\\)")))
 
@@ -550,7 +555,7 @@ ARGS are rest arguments, appended to the argument list."
 (defun swift-additions:insert-mark ()
   "Insert a mark at line."
   (interactive)
-    (insert-text-and-go-to-eol "// MARK: - "))
+  (insert-text-and-go-to-eol "// MARK: - "))
 
 (defun swift-additions:insert-todo ()
   "Insert a Todo."
@@ -562,7 +567,7 @@ ARGS are rest arguments, appended to the argument list."
   (interactive)
   (if (get-buffer xcodebuild-buffer)
       (bury-buffer xcodebuild-buffer)
-      (pop-to-buffer xcodebuild-buffer)))
+    (pop-to-buffer xcodebuild-buffer)))
 
 (defun swift-additions:get-bundle-identifier (config)
   "Get bundle identifier (as CONFIG)."
@@ -645,7 +650,7 @@ ARGS are rest arguments, appended to the argument list."
     (progn
       (let* ((choices (seq-map (lambda (item) item) list))
              (choice (completing-read title choices)))
-          (cdr (assoc choice choices))))))
+        (cdr (assoc choice choices))))))
 
 (cl-defun build-menu (&key title &key list)
   "Builds a widget menu from (as TITLE as LIST)."
@@ -655,7 +660,7 @@ ARGS are rest arguments, appended to the argument list."
     (progn
       (let* ((choices (seq-map (lambda (item) (cons item item)) list))
              (choice (completing-read title choices)))
-         (cdr (assoc choice choices))))))
+        (cdr (assoc choice choices))))))
 
 (defun swift-additions:kill-xcode-buffer ()
   "Kill the xcode buffer."
@@ -664,11 +669,7 @@ ARGS are rest arguments, appended to the argument list."
 
 (cl-defun message-with-color (&key tag &key text &key attributes)
   "Print a TAG and TEXT with ATTRIBUTES."
-  (interactive)
-  (setq-local inhibit-message nil)
-  (message "%s %s" (propertize tag 'face attributes) text)
-    (if (not DEBUG)
-        (setq-local inhibit-message t)))
+  (message "%s %s" (propertize tag 'face attributes) text))
 
 (defun is-xcodeproject ()
   "Check if its an xcode-project."
@@ -683,7 +684,7 @@ ARGS are rest arguments, appended to the argument list."
     (file-exists-p "Package.swift")))
 
 (defun swift-additions:check-for-spm-build-errors (text)
-    "Check for Swift package build erros in TEXT."
+  "Check for Swift package build erros in TEXT."
   (when DEBUG (message text))
   (if (or
        (string-match-p (regexp-quote "BUILD FAILED") text)
@@ -692,7 +693,7 @@ ARGS are rest arguments, appended to the argument list."
       (progn
         (parse-errors-from :text text)
         (when (not (string-match-p (regexp-quote "error:") text))
-         (shell-command "swift run" xcodebuild-buffer)))
+          (shell-command "swift run" xcodebuild-buffer)))
     (shell-command "swift run" xcodebuild-buffer)))
 
 (defun swift-additions:build-swift-package ()
@@ -709,7 +710,7 @@ ARGS are rest arguments, appended to the argument list."
     (async-shell-command-to-string :process-name "periphery" :command "swift test" :callback #'swift-additions:check-for-spm-build-errors)
     (message-with-color :tag "[Testing Package]" :text (format "%s. Please wait. Patience is a virtue!" (projectile-project-root)) :attributes 'warning)))
 
-; Taken from  https://gitlab.com/woolsweater/dotemacs.d/-/blob/main/modules/my-swift-mode.el
+                                        ; Taken from  https://gitlab.com/woolsweater/dotemacs.d/-/blob/main/modules/my-swift-mode.el
 (defun swift-additions:split-func-list ()
   "While on either the header of a function-like declaration or a
 call to a function, split each parameter/argument to its own
@@ -829,14 +830,14 @@ line."
     (format "%s-%s-ios%s-simulator" arch vendor version)))
 
 (defun sourcekit-lsp-arguments ()
-    "Get the lsp arguments to support UIKit."
-    (let* ((sdk (current-simulator-sdk-path))
-           (target (current-simulator-sdk)))
-      (list
-        "-Xswiftc" "-sdk"
-        "-Xswiftc" sdk
-        "-Xswiftc" "-target"
-        "-Xswiftc" target)))
+  "Get the lsp arguments to support UIKit."
+  (let* ((sdk (current-simulator-sdk-path))
+         (target (current-simulator-sdk)))
+    (list
+     "-Xswiftc" "-sdk"
+     "-Xswiftc" sdk
+     "-Xswiftc" "-target"
+     "-Xswiftc" target)))
 
 (defun my-swift-mode:eglot-server-contact (_ignored)
   "Construct the list that eglot needs to start sourcekit-lsp."
